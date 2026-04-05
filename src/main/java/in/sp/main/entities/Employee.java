dcba54a2010e00965ef74219e5af5bec9a6bac23
singhgrm <gplgrm98@gmail.com>
2026-04-06 01:55:32 +0530

    emp added

---
+++ src/main/java/in/sp/main/entities/Employee.java
@@ -0,0 +1,32 @@
+package in.sp.main.entities;
+
+public class Employee {
+    int id;
+    String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    String position;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
